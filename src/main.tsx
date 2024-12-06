2de4e596a946ded27b3cae358698b9e99ed0aaa0
bryht <bryht@live.com>
2024-12-06 16:56:46 +0100

    wip

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { TrayIcon } from '@tauri-apps/api/tray';
+import { Menu } from '@tauri-apps/api/menu';
+import { defaultWindowIcon } from '@tauri-apps/api/app';
+
+
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
+
+
+const menu = await Menu.new({
+  items: [
+    {
+      id: 'quit',
+      text: 'Quit',
+      action: () => { alert('Quit') },
+    },
+  ],
+});
+
+
+await TrayIcon.new({
+  // icon: await defaultWindowIcon() ?? '',
+  menu: menu,
+  menuOnLeftClick: true,
+});
